4b100a61a9b3130cf269e8ce3bfd985fdf989b7e
mohd-aatif-07 <atifansarifff@gmail.com>
2026-03-31 18:36:35 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newr version
+
+// alert("aatif irfan") // we are using node.js , not browser
+
+console.log(10 
+    + 10); // code redability shoud be high
+
+//Data Types in js
+/*
+1. Number => it has range like 2 to power 53
+2. bingint => use for large number 
+3. string => use for write anything in double code or single code like this "aatif"
+4. boolean => true/false;
+5. undefined => use for when you onle declare not define as value like string aatif; only declaration not defined
+6. symbol  => unique;
+7. null => means you hava variable that has no value or wait until data not come
+
+*/
+
+//object
+console.log(typeof null); //object
+console.log(typeof undefined) // undefined
+
